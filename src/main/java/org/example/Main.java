f5d0ba789d86a971d93310b57940d66ed86179d3
Khal01 <aliyev_56@list.ru>
2024-05-31 14:28:48 +0300

    .

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -6,6 +6,7 @@ import java.util.stream.Collectors;
 
 public class Main {
     public static void main(String[] args) {
+        System.out.println("Задача");
         String [] strings= {"Mercedes", "BMW", "Mercedes", "Fiat","BMW"};
         String result= Arrays.stream(strings)
                 .distinct()
