96701ef67a390d61a0b4a651ef6af0dfb8d9305a
Khal01 <aliyev_56@list.ru>
2024-05-31 14:26:46 +0300

    zadaca

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,16 @@
+package org.example;
+
+
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
+public class Main {
+    public static void main(String[] args) {
+        String [] strings= {"Mercedes", "BMW", "Mercedes", "Fiat","BMW"};
+        String result= Arrays.stream(strings)
+                .distinct()
+                .collect(Collectors.joining(" "));
+        System.out.println(result);
+
+    }
+}
